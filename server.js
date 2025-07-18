caf7340fe281e3f7286da46fced4839c14beb9e6
Rathwa Ronak <ronakrathwa69@gmail.com>
2025-07-18 09:28:29 +0530

    make project in structure way

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log('hello');
+  const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the root
+app.use(express.static(path.join(__dirname)));
+
+// Default route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'restcountry.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
